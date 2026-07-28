f57f6a59f40439669f7144e8eda22c649ad7cd8b
drorh848 <drorhh0@gmail.com>
2026-07-28 18:48:53 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* ניהול זיהוי — Service Worker v1.9 */
-const CACHE = 'zihuy-v10';
+/* ניהול זיהוי — Service Worker v1.10 */
+const CACHE = 'zihuy-v11';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
